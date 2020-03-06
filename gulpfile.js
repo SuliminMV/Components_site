a8fa1caa732aa4a2513919c10cb1f84b7489e802
Sulimin Maksim <vip-max2007@mail.ru>
2020-03-07 00:00:34 +0300

    Внесены исправления

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,22 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var server = require('browser-sync').create();
 
 gulp.task('css',function () {
     return gulp.src('sass/style.scss')
         .pipe(sass())
         .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('server',function () {
+    server.init({
+        server:'.',
+        notify:false
+    });
+
+    gulp.watch('sass/*.scss', gulp.series('css')).on('change',server.reload);
+    gulp.watch('*.html').on('change',server.reload);
+
+
 });
